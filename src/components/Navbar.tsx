cbff6c052726c28c09593b62c3168a8618f9d5bf
Matt K <matthew.b.kurtti@gmail.com>
2025-05-09 16:34:00 +0900

    chore: comment out unused future features for now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
-
 interface NavbarProps {
   theme: string;
   setTheme: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Navbar: React.FC<NavbarProps> = (props) => {
-  const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
+const Navbar: React.FC<NavbarProps> = () => {
+  // These features below will be implemented in the future
+  // const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
 
-  const handleChangeTheme = () => {
-    if (props.theme === "dark") {
-      props.setTheme("light");
-      return;
-    }
-    if (props.theme === "light") {
-      props.setTheme("dark");
-      return;
-    }
-  };
+  // const handleChangeTheme = () => {
+  //   if (props.theme === "dark") {
+  //     props.setTheme("light");
+  //     return;
+  //   }
+  //   if (props.theme === "light") {
+  //     props.setTheme("dark");
+  //     return;
+  //   }
+  // };
 
   return (
     <>
