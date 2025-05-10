12cbbf5eba6c68f51a070bcdb8815c34e0d29a8b
Matt K <matthew.b.kurtti@gmail.com>
2025-05-10 16:31:16 +0900

    fix: remove opacity in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <>
-      <div className="flex justify-center bg-[url(/mountain-pic.JPG)] bg-left bg-cover w-full h-[90vh] bg-light-background dark:bg-dark-background opacity-85">
+      <div className="flex justify-center bg-[url(/mountain-pic.JPG)] bg-left bg-cover w-full h-[90vh] bg-light-background dark:bg-dark-background">
         <h1 className="mt-3 text-6xl">Hi, I'm Matt</h1>
       </div>
     </>
