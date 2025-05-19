847dd4dc2c25e1d4cca3628ffb4e65b2e1bdf880
Matt K <matthew.b.kurtti@gmail.com>
2025-05-19 11:31:12 +0900

    fix: change desktop project spacing from justify between to normal rows

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,99 +2,98 @@ const Projects = () => {
   return (
     <>
       <div className="w-full bg-light-background-200 flex flex-col items-center">
-        <h1 className="m-2 text-3xl font-bold underline">Projects</h1>
-        <div className="w-full flex flex-col items-center sm:flex-row sm:justify-around">
-          <div className="flex flex-col items-center">
-            <h1 className="text-2xl font-bold">Team</h1>
-            <div className="m-2 p-2 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
-              <a
-                className="flex flex-col items-center"
-                href="https://www.arcane-kitchen.com/"
-                target="_blank"
-              >
-                <img
-                  className="w-1/2 h-auto"
-                  src="/dragon_orange_neutral.png"
-                  alt=""
-                />
-              </a>
+        <h1 className="m-2 mt-3 text-3xl font-bold underline">Projects</h1>
+        <h1 className="mt-3 text-2xl font-bold">Team</h1>
+        <div className="mt-5 flex flex-col items-center sm:flex-row sm:justify-between">
+          <div className="m-2 sm:mr-8 p-2 w-[40vh] flex flex-col items-center justify-center border-solid border-1 border-black">
+            <a
+              className="flex flex-col items-center"
+              href="https://www.findadoc.jp/"
+              target="_blank"
+            >
+              <img
+                className="w-1/2 h-auto"
+                src="/find_a_doc_japan_logo.jpg"
+                alt=""
+              />
+            </a>
 
-              <h2 className="font-bold">Arcane Kitchen</h2>
-              <p>
-                Arcane Kitchen is a fantasy RPG themed meal planning app that
-                aims to motivate users to eat healthy in a fun and engaging way.
-              </p>
-              <a
-                className="mt-3 flex flex-col items-center"
-                href="https://github.com/Arcane-Kitchen"
-                target="_blank"
-              >
-                <img className="w-1/6 h-auto" src="/github(1).png" alt="" />
-              </a>
-            </div>
-            <div className="m-2 p-2 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
-              <a
-                className="flex flex-col items-center"
-                href="https://www.findadoc.jp/"
-                target="_blank"
-              >
-                <img
-                  className="w-1/2 h-auto"
-                  src="/find_a_doc_japan_logo.jpg"
-                  alt=""
-                />
-              </a>
-
-              <h2 className="font-bold">Find a Doc, Japan</h2>
-              <p>
-                Find a Doc, Japan is an NPO that helps people find healthcare in
-                Japan in their language.
-              </p>
-              <a
-                className="mt-3 flex flex-col items-center"
-                href="https://github.com/ourjapanlife"
-                target="_blank"
-              >
-                <img className="w-1/6 h-auto" src="/github(1).png" alt="" />
-              </a>
-            </div>
+            <h2 className="font-bold">Find a Doc, Japan</h2>
+            <p>
+              Find a Doc, Japan is an NPO that helps people find healthcare in
+              Japan in their language.
+            </p>
+            <a
+              className="mt-3 flex flex-col items-center"
+              href="https://github.com/ourjapanlife"
+              target="_blank"
+            >
+              <img className="w-1/6 h-auto" src="/github(1).png" alt="" />
+            </a>
           </div>
-          <div className="flex flex-col items-center">
-            <h1 className="text-2xl font-bold">Solo</h1>
+          <div className="m-2 p-2 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
+            <a
+              className="flex flex-col items-center"
+              href="https://www.arcane-kitchen.com/"
+              target="_blank"
+            >
+              <img
+                className="w-1/2 h-auto"
+                src="/dragon_orange_neutral.png"
+                alt=""
+              />
+            </a>
 
-            <div className="m-2 p-2 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
-              <img className="w-1/4 h-auto" src="/strawberry.png" alt="" />
-              <h2 className="font-bold">IchiGo</h2>
-              <p>
-                A game where you are so excited to eat strawberries that you
-                just keep running faster and faster! But be careful not to hit
-                the other runners! See how long you can keep running in Ichi Go!
-              </p>
-              <a
-                className="mt-3 flex flex-col items-center"
-                href="https://github.com/matthewkurtti/IchiGo"
-                target="_blank"
-              >
-                <img className="w-1/6 h-auto" src="/github(1).png" alt="" />
-              </a>
-            </div>
-            <div className="m-2 p-2 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
-              <img className="w-1/3 h-auto" src="/flashcard.png" alt="" />
-              <h2 className="font-bold">GoRaku</h2>
-              <p>
-                Aims to assist language learners who enjoy watching shows or
-                anime in their target language. GoRaku can take an audio clip in
-                the target language from your favorite show and automatically
-                turn that audio into a text flash card with a front and back.
-              </p>
-              <a
-                className="mt-3 flex flex-col items-center"
-                href="https://github.com/matthewkurtti/goraku"
-                target="_blank"
-              >
-                <img className="w-1/6 h-auto" src="/github(1).png" alt="" />
-              </a>
-            </div>
+            <h2 className="font-bold">Arcane Kitchen</h2>
+            <p>
+              Arcane Kitchen is a fantasy RPG themed meal planning app that aims
+              to motivate users to eat healthy in a fun and engaging way.
+            </p>
+            <a
+              className="mt-3 flex flex-col items-center"
+              href="https://github.com/Arcane-Kitchen"
+              target="_blank"
+            >
+              <img className="w-1/6 h-auto" src="/github(1).png" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <h1 className="mt-4 text-2xl font-bold">Solo</h1>
+
+        <div className="mt-4 mb-4 flex flex-col items-center sm:flex-row sm:justify-between">
+          <div className="sm:mr-8 m-2 p-2 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
+            <img className="w-1/4 h-auto" src="/strawberry.png" alt="" />
+            <h2 className="font-bold">IchiGo</h2>
+            <p>
+              A game where you are so excited to eat strawberries that you just
+              keep running faster and faster! But be careful not to hit the
+              other runners! See how long you can keep running in Ichi Go!
+            </p>
+            <a
+              className="mt-3 flex flex-col items-center"
+              href="https://github.com/matthewkurtti/IchiGo"
+              target="_blank"
+            >
+              <img className="w-1/6 h-auto" src="/github(1).png" alt="" />
+            </a>
+          </div>
+          <div className="m-2 p-2 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
+            <img className="w-1/3 h-auto" src="/flashcard.png" alt="" />
+            <h2 className="font-bold">GoRaku</h2>
+            <p>
+              Aims to assist language learners who enjoy watching shows or anime
+              in their target language. GoRaku can take an audio clip in the
+              target language from your favorite show and automatically turn
+              that audio into a text flash card with a front and back.
+            </p>
+            <a
+              className="mt-3 flex flex-col items-center"
+              href="https://github.com/matthewkurtti/goraku"
+              target="_blank"
+            >
+              <img className="w-1/6 h-auto" src="/github(1).png" alt="" />
+            </a>
           </div>
         </div>
       </div>
