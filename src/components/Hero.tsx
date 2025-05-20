68117f5a87dde14b00c1dd2a25937ef035079052
Matt K <matthew.b.kurtti@gmail.com>
2025-05-20 11:51:01 +0900

    feat: init i18config for jp-en localization and use translatable string in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,10 @@
+import i18n from '../i18nconfig';
+
 const Hero = () => {
   return (
     <>
       <div className="flex justify-center bg-[url(/mountain-pic.JPG)] bg-left bg-cover w-full h-[90vh] bg-light-background dark:bg-dark-background">
-        <h1 className="mt-3 text-6xl">Hi, I'm Matt</h1>
+        <h1 className="mt-3 text-6xl">{i18n.t('hero.greeting')} </h1>
       </div>
     </>
   );
