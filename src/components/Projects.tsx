4ac1af77c60fb4e119eb8a0a9e21bf45d9b32269
Matt K <matthew.b.kurtti@gmail.com>
2025-05-23 20:12:30 +0900

    fix: add links to project titles

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,65 +1,72 @@
 // import i18n from '../i18nconfig';
-import { useTranslation } from 'react-i18next';
-
+import { useTranslation } from "react-i18next";
 
 const Projects = () => {
   const { t } = useTranslation();
 
-
   return (
     <>
       <div className="w-full bg-light-background-200 flex flex-col items-center border-solid border-1 border-black">
-        <h1 className="m-2 mt-3 text-3xl font-bold underline"> {t("projects.title")} </h1>
+        <h1 className="m-2 mt-3 text-3xl font-bold underline">
+          {" "}
+          {t("projects.title")}{" "}
+        </h1>
         <h1 className="mt-3 text-2xl font-bold"> {t("projects.team")} </h1>
         <div className="mt-5 flex flex-col items-center sm:flex-row sm:justify-between">
           <div className="m-2 sm:mr-8 p-4 w-[40vh] flex flex-col items-center justify-center border-solid border-1 border-black">
+            <img
+              className="m-2 w-1/3 h-auto"
+              src="/find_a_doc_japan_logo.jpg"
+              alt=""
+            />
             <a
-              className="flex flex-col items-center"
+              className="m-2 flex flex-col items-center font-bold underline text-blue-600 hover:text-blue-800"
               href="https://www.findadoc.jp/"
               target="_blank"
             >
-              <img
-                className="w-1/2 h-auto"
-                src="/find_a_doc_japan_logo.jpg"
-                alt=""
-              />
+              {t("projects.findadoc-title")}
             </a>
 
-            <h2 className="font-bold"> {t("projects.findadoc-title")} </h2>
-            <p>
-              {t("projects.findadoc-description")}
-            </p>
+            {/* <h2 className="font-bold"> </h2> */}
+            <p>{t("projects.findadoc-description")}</p>
             <a
               className="mt-3 flex flex-col items-center"
               href="https://github.com/ourjapanlife"
               target="_blank"
             >
-              <img className="w-1/6 h-auto hover:bg-light-accent-300" src="/github(1).png" alt="" />
-            </a>
-          </div>
-          <div className="m-2 p-4 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
-            <a
-              className="flex flex-col items-center"
-              href="https://www.arcane-kitchen.com/"
-              target="_blank"
-            >
               <img
-                className="w-1/2 h-auto"
-                src="/dragon_orange_neutral.png"
+                className="w-1/6 h-auto hover:bg-light-accent-300"
+                src="/github(1).png"
                 alt=""
               />
             </a>
+          </div>
+          <div className="m-2 p-4 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
+            <img
+              className="w-1/2 h-auto"
+              src="/dragon_orange_neutral.png"
+              alt=""
+            />
+            <a
+              className="m-2 flex flex-col items-center font-bold underline text-blue-600 hover:text-blue-800"
+              href="https://www.arcane-kitchen.com/"
+              target="_blank"
+            >
+              {t("projects.arcane-kitchen-title")}
+            </a>
 
-            <h2 className="font-bold"> {t("projects.arcane-kitchen-title")} </h2>
-            <p>
-              {t("projects.arcane-kitchen-description")}
-            </p>
+            {/* <h2 className="font-bold"> </h2> */}
+            <p>{t("projects.arcane-kitchen-description")}</p>
             <a
               className="mt-3 flex flex-col items-center"
               href="https://github.com/Arcane-Kitchen"
               target="_blank"
             >
-              <img className="w-1/6 h-auto hover:bg-light-accent-300" src="/github(1).png" alt="" />
+              <img
+                className="w-1/6 h-auto hover:bg-light-accent-300"
+                src="/github(1).png"
+                alt=""
+              />
             </a>
           </div>
         </div>
@@ -68,31 +75,35 @@ const Projects = () => {
 
         <div className="mt-4 mb-4 flex flex-col items-center sm:flex-row sm:justify-between">
           <div className="sm:mr-8 m-2 p-4 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
-            <img className="w-1/4 h-auto" src="/strawberry.png" alt="" />
-            <h2 className="font-bold"> {t("projects.ichigo-title")} </h2>
-            <p>
-              {t("projects.ichigo-description")}
-            </p>
+            <img className="m-2 w-1/4 h-auto" src="/strawberry.png" alt="" />
+            <h2 className="m-2 font-bold"> {t("projects.ichigo-title")} </h2>
+            <p className="m-2">{t("projects.ichigo-description")}</p>
             <a
               className="mt-3 flex flex-col items-center"
               href="https://github.com/matthewkurtti/IchiGo"
               target="_blank"
             >
-              <img className="w-1/6 h-auto hover:bg-light-accent-300" src="/github(1).png" alt="" />
+              <img
+                className="w-1/6 h-auto hover:bg-light-accent-300"
+                src="/github(1).png"
+                alt=""
+              />
             </a>
           </div>
           <div className="m-2 p-4 w-[40vh] flex flex-col items-center border-solid border-1 border-black">
-            <img className="w-1/3 h-auto" src="/flashcard.png" alt="" />
-            <h2 className="font-bold"> {t("projects.goraku-title")} </h2>
-            <p>
-              {t("projects.goraku-description")}
-            </p>
+            <img className="m-2 w-1/3 h-auto" src="/flashcard.png" alt="" />
+            <h2 className="m-2 font-bold"> {t("projects.goraku-title")} </h2>
+            <p className="m-2">{t("projects.goraku-description")}</p>
             <a
               className="mt-3 flex flex-col items-center"
               href="https://github.com/matthewkurtti/goraku"
               target="_blank"
             >
-              <img className="w-1/6 h-auto hover:bg-light-accent-300" src="/github(1).png" alt="" />
+              <img
+                className="w-1/6 h-auto hover:bg-light-accent-300"
+                src="/github(1).png"
+                alt=""
+              />
             </a>
           </div>
         </div>
