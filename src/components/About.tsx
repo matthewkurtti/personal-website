885321b31f8705886c8484ff5c69eb6e028e63be
Matt K <matthew.b.kurtti@gmail.com>
2025-05-23 20:02:17 +0900

    feat: make pulse effect for findadoc in about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,17 @@
 // import i18n from '../i18nconfig';
-import { useTranslation } from 'react-i18next';
-
+import { useTranslation } from "react-i18next";
 
 const About = () => {
   const { t } = useTranslation();
-
 
   return (
     <>
       <div className="w-full flex flex-col-reverse items-center lg:flex-row lg:justify-around lg:items-center bg-light-background-100 border-solid border-1 border-black">
         <div className="mt-5 mb-5 flex flex-col items-center justify-center">
-          <h1 className="m-1 p-1 text-3xl font-bold underline"> {t("about.title")} </h1>
+          <h1 className="m-1 p-1 text-3xl font-bold underline">
+            {" "}
+            {t("about.title")}{" "}
+          </h1>
           <p className="m-1 p-1 text-2xl">
             {t("about.from")}🗽 {t("about.live")} 🗾
           </p>
@@ -18,9 +19,9 @@ const About = () => {
             {t("about.university")} ⚛️ {t("about.teach")} 👨🏼‍🏫
           </p>
           <p className="m-1 p-1 text-2xl">
-            {t("about.engineer1")} -actively contributing on {" "}
+            {t("about.engineer1")} -actively contributing on{" "}
             <a
-              className="text-light-accent-500 hover:text-light-accent-700 underline"
+              className="text-blue-600 hover:text-blue-800 animate-pulse underline"
               href="https://www.findadoc.jp"
               target="_blank"
             >
@@ -28,12 +29,8 @@ const About = () => {
             </a>
             -{t("about.engineer2")} 🧑🏼‍💻
           </p>
-          <p className="m-1 p-1 text-2xl">
-            {t("about.hobby")} 🏃🏼‍♂️ 🚴‍♂️ 🌳
-          </p>
-          <p className="m-1 p-1 text-2xl">
-            {t("about.collaborate")} 🤝
-          </p>
+          <p className="m-1 p-1 text-2xl">{t("about.hobby")} 🏃🏼‍♂️ 🚴‍♂️ 🌳</p>
+          <p className="m-1 p-1 text-2xl">{t("about.collaborate")} 🤝</p>
         </div>
         <img
           className="mt-4 mb-4 w-[40vh] h-auto border-black border-solid border-1"
