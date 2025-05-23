282824963188091de73a45c91c0648d6fe6cf099
Matt K <matthew.b.kurtti@gmail.com>
2025-05-23 10:01:59 +0900

    feat: hamburger menu that closes when clicked outside

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import i18n from '../i18nconfig';
-import { useTranslation } from 'react-i18next';
+import { useEffect, useRef, useState } from "react";
+import i18n from "../i18nconfig";
+import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   theme: string;
@@ -10,84 +10,189 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = () => {
   const { t } = useTranslation();
   const [selectedLanguage, setSelectedLanguage] = useState("en");
+  const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
+  // uses local storage to check user's previously chosen language
   useEffect(() => {
-    const currentLanguage = localStorage.getItem('i18nextLng');
-    if(currentLanguage === 'en'){
-      setSelectedLanguage('en');
+    const currentLanguage = localStorage.getItem("i18nextLng");
+    if (currentLanguage === "en") {
+      setSelectedLanguage("en");
     }
-    if(currentLanguage === 'jp'){
-      setSelectedLanguage('jp');
+    if (currentLanguage === "jp") {
+      setSelectedLanguage("jp");
     }
-  }, [])
+  }, []);
 
-  const handleChangeLanguage =  () => {
-    if(selectedLanguage === 'en'){
-      i18n.changeLanguage('jp');
-      localStorage.setItem('i18nextLng', 'jp');
-      setSelectedLanguage('jp');
-    }
-    if(selectedLanguage === 'jp'){
-      i18n.changeLanguage('en');
-      localStorage.setItem('i18nextLng', 'en');
-      setSelectedLanguage('en');
-    }
-  }
-  // These features below will be implemented in the future
-  // const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
+  // close hamburger menu if clicked outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setHamburgerOpen(false);
+      }
+    };
 
-  // const handleChangeTheme = () => {
-  //   if (props.theme === "dark") {
-  //     props.setTheme("light");
-  //     return;
-  //   }
-  //   if (props.theme === "light") {
-  //     props.setTheme("dark");
-  //     return;
-  //   }
-  // };
+    if (hamburgerOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [hamburgerOpen]);
+
+  const handleChangeLanguage = () => {
+    if (selectedLanguage === "en") {
+      i18n.changeLanguage("jp");
+      localStorage.setItem("i18nextLng", "jp");
+      setSelectedLanguage("jp");
+    }
+    if (selectedLanguage === "jp") {
+      i18n.changeLanguage("en");
+      localStorage.setItem("i18nextLng", "en");
+      setSelectedLanguage("en");
+    }
+  };
+
+  const toggleMenu = () => setHamburgerOpen(!hamburgerOpen);
 
   return (
     <>
-        <div className="shadow-lg fixed top-0 left-0 z-1000 bg-light-background-50 dark:bg-dark-background-50 w-full h-[10vh] p-2 flex justify-between border-b-solid border-b-1 border-b-light-accent-950 dark:border-b-dark-accent-950">
-          <a className='ml-2 flex justify-center w-1/12' href="#hero">
-            <img className='cursor-pointer w-full h-full' src="logo2.png" alt="" />
+      <div className="shadow-lg fixed top-0 left-0 z-1000 bg-light-background-50 dark:bg-dark-background-50 w-full h-[10vh] p-2 flex justify-between items-center border-b-solid border-b-1 border-b-light-accent-950 dark:border-b-dark-accent-950">
+        <a
+          className="ml-2 flex justify-center w-1/7 h-2/3 md:w-1/14 md:h-4/5"
+          href="#hero"
+        >
+          <img
+            className="cursor-pointer w-full h-full"
+            src="logo2.png"
+            alt=""
+          />
+        </a>
 
+        {/* Vertical Menu Items for Mobile */}
+        <div
+          ref={menuRef}
+          className={`md:hidden flex flex-col items-start mt-34 bg-light-background-50 px-4 transition-all duration-300 ease-in-out overflow-hidden ${
+            hamburgerOpen ? "max-h-60 py-2" : "max-h-0"
+          }`}
+        >
+          <a
+            href="#about"
+            className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
+          >
+            {t("navbar.about")}
           </a>
-          <div className="flex items-center">
-            <a
-              href="#about"
-              className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
-            >
-              {t("navbar.about")}
-            </a>
-            <a
-              href="#skills"
-              className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
-            >
-              {t("navbar.skills")}
-            </a>
-            <a
-              href="#projects"
-              className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
-            >
-              {t("navbar.projects")}
-            </a>
-            {/* <button className='bg-light-background-200 p-2' onClick={() => {handleChangeLanguage()}}>EN ➡️ 日本語 </button> */}
-            {selectedLanguage === 'en' && (<button className='mr-4 p-2 cursor-pointer border-solid border-1 border-black hover:text-light-accent-300 hover:border-light-accent-300' onClick={() => {handleChangeLanguage()}}>EN ➡️ 日本語</button>)}
-            {selectedLanguage === 'jp' && (<button className='mr-4 p-2 cursor-pointer border-solid border-1 border-black hover:text-light-accent-300 hover:border-light-accent-300' onClick={() => {handleChangeLanguage()}}>日本語 ➡️ EN</button>)}
-
-            {/* temporarily removing theme changer until it is optimized later */}
-            {/* <button
-              className="mr-2 p-1 bg-light-accent dark:bg-dark-accent text-light-text dark:text-dark-text border-solid border-1 border-light-accent-950 dark:border-dark-accent-950"
+          <a
+            href="#skills"
+            className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
+          >
+            {t("navbar.skills")}
+          </a>
+          <a
+            href="#projects"
+            className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
+          >
+            {t("navbar.projects")}
+          </a>
+          {selectedLanguage === "en" && (
+            <button
+              className="mr-4 p-2 cursor-pointer border-solid border-1 border-black hover:text-light-accent-300 hover:border-light-accent-300"
               onClick={() => {
-                handleChangeTheme();
+                handleChangeLanguage();
               }}
             >
-              Change theme
-            </button> */}
-          </div>
+              EN ➡️ 日本語
+            </button>
+          )}
+          {selectedLanguage === "jp" && (
+            <button
+              className="mr-4 p-2 cursor-pointer border-solid border-1 border-black hover:text-light-accent-300 hover:border-light-accent-300"
+              onClick={() => {
+                handleChangeLanguage();
+              }}
+            >
+              日本語 ➡️ EN
+            </button>
+          )}
         </div>
+
+        {/* Horizontal menu for desktop */}
+        <div className="hidden md:flex md:items-center">
+          <a
+            href="#about"
+            className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
+          >
+            {t("navbar.about")}
+          </a>
+          <a
+            href="#skills"
+            className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
+          >
+            {t("navbar.skills")}
+          </a>
+          <a
+            href="#projects"
+            className="mr-8 p-2 cursor-pointer text-2xl text-light-text dark:text-dark-text hover:text-light-accent-300"
+          >
+            {t("navbar.projects")}
+          </a>
+          {selectedLanguage === "en" && (
+            <button
+              className="mr-4 p-2 cursor-pointer border-solid border-1 border-black hover:text-light-accent-300 hover:border-light-accent-300"
+              onClick={() => {
+                handleChangeLanguage();
+              }}
+            >
+              EN ➡️ 日本語
+            </button>
+          )}
+          {selectedLanguage === "jp" && (
+            <button
+              className="mr-4 p-2 cursor-pointer border-solid border-1 border-black hover:text-light-accent-300 hover:border-light-accent-300"
+              onClick={() => {
+                handleChangeLanguage();
+              }}
+            >
+              日本語 ➡️ EN
+            </button>
+          )}
+        </div>
+
+        {/* Hamburger Button */}
+        <div className="md:hidden mr-2">
+          <button
+            onClick={toggleMenu}
+            className="text-gray-700 focus:outline-none"
+          >
+            <svg
+              className="w-9 h-9"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {hamburgerOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+      </div>
     </>
   );
 };
