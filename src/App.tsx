2103ce17b8d06f66e9a24a0c830f99129fb36fdf
Matt K <matthew.b.kurtti@gmail.com>
2025-05-25 20:59:36 +0900

    feat: basic loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
-// import i18n from "./i18nconfig";
+import LoadingScreen from "./components/LoadingScreen";
 
-function App() {
+const App: React.FC = () => {
   const [theme, setTheme] = useState("light");
+  const [isLoading, setIsLoading] = useState(true);
 
-  // const handleChangeLanguage =  () => {
-  //   i18n.changeLanguage('jp')
-  // }
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
-      <Navbar theme={theme} setTheme={setTheme} />
-      <div
-        className="mt-15 md:mt-22 bg-light-background dark:bg-dark-background"
-        data-theme={theme}
-      >
-        <div id="hero">
-          <Hero />
-        </div>
-        <div id="about">
-          <About />
-        </div>
-        <div id="skills">
-          <Skills />
-        </div>
-        <div id="projects">
-          <Projects />
-        </div>
-        <Footer />
-      </div>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <Navbar theme={theme} setTheme={setTheme} />
+          <div
+            className="mt-15 md:mt-22 bg-light-background dark:bg-dark-background"
+            data-theme={theme}
+          >
+            <div id="hero">
+              <Hero />
+            </div>
+            <div id="about">
+              <About />
+            </div>
+            <div id="skills">
+              <Skills />
+            </div>
+            <div id="projects">
+              <Projects />
+            </div>
+            <Footer />
+          </div>
+        </>
+      )}
     </>
   );
-}
+};
 
 export default App;
